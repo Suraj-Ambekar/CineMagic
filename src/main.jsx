c3f77655a16fe6a8fea4273bbe492a8266de4080
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-09 08:59:02 +0530

    created redux store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />)
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
 
 //   <React.StrictMode>
