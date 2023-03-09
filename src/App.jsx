91a426cd2803c1e2b6f18213cb65e634d5ce9ed6
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-09 09:29:43 +0530

    checked working of redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react';
 
 import { fetchDataFromApi } from './utils/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const {url} = useSelector((state)=> state.home );
+  console.log(url);
 
   useEffect(()=>{
     apiTesting()
@@ -10,13 +16,17 @@ function App() {
 
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular")
-      .then((res)=>{console.log(res);});
+      .then((res)=>{
+        console.log(res);
+        dispatch(getApiConfiguration(res));
+      });
   }
 
 
   return (
     <div className="App">
-      App
+      <h1>App</h1>
+      <h3>{url.results[0].original_title}</h3>
     </div>
   )
 }
