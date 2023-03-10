c690d7c71874bc3e7120d0ee905b059e9557b74f
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-10 15:34:52 +0530

    added new logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,8 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/logo (1).jpg";
+// import logo from "../../assets/logo (1).jpg";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
     const [show, setShow] = useState("top");
