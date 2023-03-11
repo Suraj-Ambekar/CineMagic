08b3a39095487162cf0d2cffee333bb592319b61
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-11 10:12:47 +0530

    created basic structure of switch tab

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles.scss";
 
 const SwitchTabs = ({data, onTabChange}) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [left, setLeft] = useState(0);
+
   return (
     <div className='switchingTabs'>
-      SwitchTabs
+      <div className='tabItems'>
+        {data.map((tab, index)=> {
+          <span key={index} className={`tabItem`}>
+            {tab}
+          </span>
+        })}
+      </div>
+      <span className='movingBg' style={{left}} />
     </div>
   )
 }
