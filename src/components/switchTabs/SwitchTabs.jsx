73ba76b84693ac8469f7d55a625c714f293b0b5d
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-11 09:11:34 +0530

    added new component switch tabs

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./styles.scss";
+
+const SwitchTabs = ({data, onTabChange}) => {
+  return (
+    <div className='switchingTabs'>
+      SwitchTabs
+    </div>
+  )
+}
+export default SwitchTabs
