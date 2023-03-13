92fc75153467045c39fcac1f699543412d1d172f
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-13 08:00:23 +0530

    added switch tab effect

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
-import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+import React, { useState } from "react";
+
+// import Carousel from "../../../components/carousel/Carousel";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
-  const onTabChange = (tab) => {
 
-  }
 
-  return (
-    <div className='carouselSection'>
-        <ContentWrapper>
-           <span className='carouselTitle'>Trending</span>
-           <SwitchTabs data={["Day","Week"]} onTabChange={onTabChange} />
-        </ContentWrapper>
-    </div>
-  )
-}
 
-export default Trending
+    const onTabChange = (tab) => {
+    };
+
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">Trending</span>
+                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default Trending;
