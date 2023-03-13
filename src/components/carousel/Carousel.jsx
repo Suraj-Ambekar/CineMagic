f91e107042b9d7ac294bbf4e309692afb8671bd7
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-13 11:39:49 +0530

    imported and used Genres component

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -11,8 +11,10 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
+import Geners from "../genres/Geners";
 
 import "./style.scss";
+
 
 const Carousel = ({data, loading}) => {
     const carouselContainer = useRef();
@@ -68,6 +70,7 @@ const Carousel = ({data, loading}) => {
                                         <CircleRating 
                                             rating={item.vote_average.toFixed(1)}
                                         />
+                                        <Geners data={item.genre_ids.slice(0,2)} />
                                     </div>
                                     <div className="textBlock">
                                         <span className="title">
