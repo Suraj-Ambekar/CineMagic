13a9fe1a110eeb4fa8dc52b23b1a210d1c9e3814
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-13 11:39:17 +0530

    added logic to fetched genres

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 import { fetchDataFromApi } from './utils/api';
 import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration } from './store/homeSlice';
+import { getApiConfiguration, getGenres } from './store/homeSlice';
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -19,7 +19,8 @@ function App() {
   console.log(url);
 
   useEffect(()=>{
-    fetchApiConfig()
+    fetchApiConfig();
+    genresCall();
   },[])
 
   const fetchApiConfig = () => {
@@ -35,6 +36,24 @@ function App() {
 
         dispatch(getApiConfiguration(url));
       });
+  }
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv","movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url)=> {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    // console.log(data);
+    data.map(({genres})=> {
+      return genres.map((item)=> (allGenres[item.id]= item));
+    });
+    // console.log(allGenres);
+    dispatch(getGenres(allGenres));
   }
 
 
