8b7bd0c64c00261f6f4c72d288ca2f21b9670d46
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-13 09:20:11 +0530

    added carousel section

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
@@ -20,6 +21,7 @@ const Trending = () => {
                 <span className="carouselTitle">Trending</span>
                 <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
             </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
         </div>
     );
 };
