e989b8f4e5be1266b2d958895457f203d7022ff4
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-14 16:45:56 +0530

    added logic to fetched search query data

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import "./style.scss";
 
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import noResults from "../../assets/no-results.png";
+
 const SearchResult = () => {
+
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+
+  const fetchInitialData = () => {
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then((res)=>{
+        setData(res);
+        setPageNum((prev)=> prev +1);
+        setLoading(false);
+      });
+  };
+
+  const fetchNextPageData = () => {
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then((res)=>{
+        if(data?.results){
+          setData({...data, results: [...data?.results, ...res.results]})
+        } else {
+          setData(res)
+        }
+        setPageNum((prev)=> prev +1);
+      });      
+  };
+
+  useEffect(()=> {
+    fetchInitialData();
+  },[query]);
+
   return (
     <div>SearchResult</div>
   )
