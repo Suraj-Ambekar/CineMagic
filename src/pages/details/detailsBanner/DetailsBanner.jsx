da44beee1d9589835ee185c725670642ded7cbd9
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-14 11:57:20 +0530

    fetched movie overview, timing, data

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -66,10 +66,51 @@ const DetailsBanner = ({ video, crew }) => {
                                             <CircleRating 
                                                 rating={data.vote_average.toFixed(1)}
                                             />
-                                            <div className="playbtn">
+                                            <div className="playbtn" onClick={()=> {}}>
                                                 <PlayIcon />
+                                                <span className="text">Watch Trailer</span>
                                             </div>
-                                        </div>                                        
+                                        </div> 
+                                        <div className="overview">
+                                            <div className="heading">
+                                                Overview
+                                            </div>
+                                            <div className="description">
+                                                {data.overview}
+                                            </div>
+                                        </div>   
+                                        <div className="info">
+                                            {data.status && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Status:{""}
+                                                    </span>
+                                                    <span className="text">
+                                                        {data.status}
+                                                    </span>
+                                                </div>
+                                            )}
+                                            {data.release_date && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Release Date:{""}
+                                                    </span>
+                                                    <span className="text">
+                                                        {dayjs(data.release_date).format("MMM D, YYYY")}
+                                                    </span>
+                                                </div>
+                                            )}
+                                            {data.runtime && (
+                                                <div className="infoItem">
+                                                    <span className="text bold ">
+                                                        runtime:{""}
+                                                    </span>
+                                                    <span className="text">
+                                                        {toHoursAndMinutes(data.runtime)}
+                                                    </span>
+                                                </div>
+                                            )}
+                                        </div>                                    
                                     </div>
                                 </div>
                             </ContentWrapper>
