07d55c0c7f23309a37ef38d44e4f2222c6a8da02
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-03-14 14:06:36 +0530

    added cast component to details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import "./style.scss";
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
+import Cast from '../../components/cast/Cast';
 
 const Details = () => {
   const {mediaType, id} = useParams();
@@ -13,6 +14,7 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creaditsLoading} />
     </div>
     
   )
